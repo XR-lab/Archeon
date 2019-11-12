38b12dc565e7852fc07c3c06a3921e1276e6fca2
gwnrobin <robinschellingerhout@live.nl>
2019-11-12 12:49:02 +0100

    fire fixed

---
+++ Assets/Scripts/Crafting/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider c)
+    {
+        print(c.gameObject.name + " in");
+    }
+    private void OnTriggerExit(Collider c)
+    {
+        print(c.gameObject.name + " out");
+    }
+}
