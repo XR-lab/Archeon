2a571dfacda07516967386ca4b89edcdab0ced53
samjerry <jerry.sam@ziggo.nl>
2019-11-18 10:12:12 +0100

    Added Steering behaviour for fish AI

--- Assets/Scripts/SwimBehaviour.cs
+++ Assets/Scripts/SwimBehaviour.cs
@@ -4,37 +4,55 @@ using UnityEngine;
 
 public class SwimBehaviour : StateMachineBehaviour
 {
-    private GameObject target;
+    private float _mass = 100;
+    private float _maxVelocity = 2;
+    private float _maxForce = 15;
+
+    private Vector3 _velocity;
+    private GameObject _target;
     private Transform[] _targets;
     private Transform _destination;
     private Transform _oldDest;
-    private float _speed = 2;
     private int _destNum = 0;
 
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
-        target = GameObject.Find("Targets");
-        _targets = target.GetComponentsInChildren<Transform>();
-
+        _target = GameObject.Find("Targets");
+        _targets = _target.GetComponentsInChildren<Transform>();
+        _velocity = Vector3.zero;
         GetDestination();
     }
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
+        Vector3 _desiredVelocity = _destination.transform.position - animator.transform.position;
+        _desiredVelocity = _desiredVelocity.normalized * _maxVelocity;
+
+        Vector3 _steering = _desiredVelocity - _velocity;
+        _steering = Vector3.ClampMagnitude(_steering, _maxForce);
+        _steering /= _mass;
+
+        _velocity = Vector3.ClampMagnitude(_velocity + _steering, _maxVelocity);
+        animator.transform.position += _velocity * Time.deltaTime;
+        animator.transform.forward = _velocity.normalized;
+
+        Debug.DrawRay(animator.transform.position, _velocity.normalized * 2, Color.green);
+        Debug.DrawRay(animator.transform.position, _desiredVelocity.normalized * 2, Color.magenta);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             animator.SetBool("IsPanicing", true);
         }
 
-        animator.transform.position = Vector3.MoveTowards(animator.transform.position, _destination.position, _speed * Time.deltaTime);
+        //animator.transform.position = Vector3.MoveTowards(animator.transform.position, _destination.position, _maxVelocity * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.X))
         {
             animator.SetBool("IsPanicing", true);
         }
 
-        if (animator.transform.position == _destination.transform.position)
+        if (Vector3.Distance(animator.transform.position, _destination.transform.position) <= 0.5)
         {
             _oldDest = _destination;
             GetDestination();
@@ -45,7 +63,7 @@ public class SwimBehaviour : StateMachineBehaviour
             GetDestination();
         }
 
-        Debug.DrawLine(animator.transform.position, _destination.transform.position, Color.green);
+        //Debug.DrawLine(animator.transform.position, _destination.transform.position, Color.green);
 
     }
 
