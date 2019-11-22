9c400ab01838efbd9780e8172054e939689e1762
Vychron <21598@ma-web.nl>
2019-11-22 14:48:40 +0100

    made fish visually cling to spear when stabbed

---
+++ Assets/Scripts/CaughtBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaughtBehaviour : StateMachineBehaviour {
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) {
+        animator.StopPlayback();
+        base.OnStateEnter(animator, animatorStateInfo, layerIndex);
+    }
+}
