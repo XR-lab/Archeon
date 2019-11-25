b1c8fd06ff7a5ecddcebce0f2f60577150e0ef9b
krokoloko <daan.j.ruting@gmail.com>
2019-11-25 17:25:14 +0100

    Drawing on the height map
    needs to be optimised

--- Assets/Scripts/Ripple Effect/RippleGenerator.cs
+++ Assets/Scripts/Ripple Effect/RippleGenerator.cs
@@ -1,54 +1,194 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RippleGenerator : MonoBehaviour
 {
-    [SerializeField]
-    private Texture2D _rippleTexture;
+    public class Ripple
+    {
+        public int x
+        {
+            get { return _x; }
+            private set { _x = x; }
+        }
+        private int _x;
 
-    [SerializeField]
-    private Color _colorRangeMin;
+        public int y
+        {
+            get { return _y; }
+            private set { _y = y; }
+        }
+        private int _y;
 
-    [SerializeField]
-    private Color _colorRangeMax;
+        public Wave[] waves
+        {
+            get { return _waves; }
+            private set { _waves = waves; }
+        }
+        private Wave[] _waves;
 
+        public float dimmingRate
+        {
+            private set { _dimmingRate = dimmingRate; }
+            get { return _dimmingRate;}
+        }
+        private float _dimmingRate;
+
+        public float volume
+        {
+            private set { _volume = volume; }
+            get { _volume -= _dimmingRate; return _volume; }
+        }
+        private float _volume;
+
+        public Ripple(int cx, int cy, float volume, float dimmingRate, int radius)
+        {
+            _x = cx;
+            _y = cy;
+            _dimmingRate = dimmingRate;
+            _volume = volume;
+            _waves = new Wave[radius]; 
+            for(int i = 0; i < radius; i++)
+            {
+                Wave wave = new Wave(_volume);
+                _waves[i] = wave;
+            }
+        }
+
+        public class Wave
+        {
+            public float Volume
+            {
+                private get { return _volume; }
+                set { _volume = Volume; }
+            } 
+            private float _volume;
+
+            private float _travelledDistance;
+
+            public Wave(float volume)
+            {
+                _travelledDistance = 0;
+                _volume = volume;
+            }
+
+            public float GetVolume(float travelled)
+            {
+                _travelledDistance += travelled;
+                return _volume - _travelledDistance;
+            }
+        }
+    }
+
+    public class OnRipple : UnityEvent<Texture2D> { };
+
+    public OnRipple onRipple = new OnRipple();
+    [SerializeField]
     private int _textureHeight;
+
+    [SerializeField]
     private int _textureWidth;
+
+    [SerializeField]
+    private int _interval;
+
+    private Texture2D _sampleTexture;
+
+    private List<Ripple> _ripples;    
 
     // Start is called before the first frame update
     void Start()
     {
-        _textureHeight = _rippleTexture.height;
-        _textureHeight = _rippleTexture.width;
+        _textureWidth = _textureWidth != 0 ? _textureWidth : 400;
+        _textureHeight = _textureHeight != 0 ? _textureHeight : 400;
+        _sampleTexture = new Texture2D(_textureWidth, 
+                                       _textureHeight);
+        _ripples = new List<Ripple>();
+        Color[] heightMapDefault = new Color[_textureWidth*_textureHeight];
+
+        Color[] canvas = _sampleTexture.GetPixels();
+
+        for (int i = 0; i < canvas.Length; i++)
+        {
+            heightMapDefault[i] = new Color(0.5f, 0.5f, 0.5f);
+        }
+
+        _sampleTexture.SetPixels(heightMapDefault);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private IEnumerator GenerateRipples(int Ripples, float size, float timeOffset)
+    //Instantiates a ripple that will be drawn in the heightmap
+    public IEnumerator GenerateRipple(Vector2 position, float rippleStrength, int radius, float timeOffset)
     {
         yield return new WaitForSeconds(timeOffset);
+
+        Ripple ripple = new Ripple((int)position.x, (int)position.y, rippleStrength, 0.01f, radius);
+
+        _ripples.Add(ripple);
+        
+        StartCoroutine(DrawRipple(_ripples[_ripples.Count-1]));
     }
 
-    private void drawCircle(int x, int y, float radius, float thickness, Color color)
+    //Generates a ripple by drawing circles
+    private IEnumerator DrawRipple(Ripple ripple)
     {
-        float rSquared = radius * radius;
-
-        for (int w = 0; w < _rippleTexture.width; w++)
+        bool lowestPoint = true;
+        Ripple.Wave[] waves = ripple.waves;
+        for (int i = 0; i < waves.Length; i++)
         {
-            for (int h = 0; h < _rippleTexture.height; h++)
+            float intensity = Mathf.Sin(i*0.11f-1.8f)/Mathf.Pow(1.1f,i*0.11f-1.8f);
+
+            float rgb = (intensity + 1.156f)/ 2.021f*0.3f+0.4f ;
+
+            Debug.LogError(rgb);
+
+            Color color = new Color(rgb,rgb,rgb);
+
+            DrawCircle(ripple.x, ripple.y, i, color);
+
+            onRipple.Invoke(_sampleTexture);
+
+            if(i % _interval == 0)
+                yield return new WaitForEndOfFrame();
+        }
+
+        foreach(Ripple.Wave wave in waves)
+        {
+            if(ripple.dimmingRate <= wave.GetVolume(ripple.dimmingRate))
             {
-                if ((x - h) * (x - h) + (y - h) * (y - h) < rSquared)
-                {
-                    _rippleTexture.SetPixel(w, h, color);
-                }
+                lowestPoint = false;
+                break;
             }
         }
 
-        _rippleTexture.Apply();
+        //if (!lowestPoint)
+        //    StartCoroutine(DrawRipple(ripple));
+    }
+
+    //Draw a circle on the sampleTexture
+    private void DrawCircle(int x, int y, int r, Color color)
+    {
+        float radius = Mathf.Ceil(r / Mathf.Sqrt(2));
+        int diamater = 1 / 4 - r;
+        
+        for (int p = 0; p <= radius; p++)
+        {
+            _sampleTexture.SetPixel(x + p, y + r, color);
+            _sampleTexture.SetPixel(x + p, y - r, color);
+            _sampleTexture.SetPixel(x - p, y + r, color);
+            _sampleTexture.SetPixel(x - p, y - r, color);
+            _sampleTexture.SetPixel(x + r, y + p, color);
+            _sampleTexture.SetPixel(x + r, y - p, color);
+            _sampleTexture.SetPixel(x - r, y + p, color);
+            _sampleTexture.SetPixel(x - r, y - p, color);
+
+            diamater += 2 * p + 1;
+            if (diamater > 0)
+            {
+                diamater += 2 - 2 * r--;
+            }
+        }
+
+        _sampleTexture.Apply();
     }
 }
---
+++ Assets/Scripts/Ripple Effect/WaterInteractable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterInteractable : MonoBehaviour
+{
+
+    [SerializeField]
+    private int _waterLayer;
+
+    [SerializeField]
+    private float _rippleVolume;
+
+    [SerializeField]
+    private float _timeDelay;
+
+    [SerializeField]
+    private int _rippleRadius;
+
+    void Start()
+    {
+        _rippleRadius = _rippleRadius != 0 && _rippleRadius > 0 ? _rippleRadius : 50;
+
+        _rippleVolume = _rippleVolume > 1 ? _rippleVolume : 1; 
+        
+    }
+
+    //Trigger when water got touched
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == _waterLayer)
+        {
+            Debug.Log("water");
+
+            RippleGenerator generator = other.gameObject.GetComponent<RippleGenerator>();
+            Debug.LogError(new Vector2(
+                other.contacts[other.contactCount - 1].point.x, other.contacts[other.contactCount - 1].point.z));
+            StartCoroutine(generator.GenerateRipple(new Vector2(
+                other.contacts[other.contactCount-1].point.x, other.contacts[other.contactCount-1].point.z), 
+                _rippleVolume, _rippleRadius, _timeDelay));
+        }
+    }
+}
