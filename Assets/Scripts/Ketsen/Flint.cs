d91c1f99a1cb034aa081a42e9274c07fe04ebd68
gwnrobin <robinschellingerhout@live.nl>
2019-11-28 10:56:20 +0100

    pek fixed

--- Assets/Scripts/Ketsen/Flint.cs
+++ Assets/Scripts/Ketsen/Flint.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Flint : MonoBehaviour
 {
     [SerializeField]
-    private float _breakPower = 5;
+    private float _breakPower;
     [SerializeField]
     private GameObject _spark;
     [SerializeField]
@@ -22,9 +22,10 @@ public class Flint : MonoBehaviour
     {
         if (!_col.gameObject.CompareTag("Stone"))
             return;
-
+        //print(_RG.velocity.magnitude);
         if (_RG.velocity.magnitude > _breakPower || _col.gameObject.GetComponent<Rigidbody>().velocity.magnitude > _breakPower)
         {
+            print(_RG.velocity.magnitude);
             //Instantiate(_spark, (this.transform.position + _col.transform.position) / 2, Quaternion.identity);
             Instantiate(_flintShard[Random.Range(0, _flintShard.Count)], (this.transform.position + _col.transform.position), Quaternion.identity);
         }
