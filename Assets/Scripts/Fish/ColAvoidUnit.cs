8a0fcbac2697e06dd53a3c0dec8e3490cdee9fd6
samjerry <jerry.sam@ziggo.nl>
2019-11-28 11:46:36 +0100

    Added a Wincondition

--- Assets/Scripts/Fish/ColAvoidUnit.cs
+++ Assets/Scripts/Fish/ColAvoidUnit.cs
@@ -5,20 +5,14 @@ namespace UnityMovementAI
     public class ColAvoidUnit : MonoBehaviour
     {
 
-        public LinePath path;
-
         SteeringBasics steeringBasics;
-        FollowPath followPath;
         CollisionAvoidance colAvoid;
 
         NearSensor colAvoidSensor;
 
         void Start()
         {
-            path.CalcDistances();
-
             steeringBasics = GetComponent<SteeringBasics>();
-            followPath = GetComponent<FollowPath>();
             colAvoid = GetComponent<CollisionAvoidance>();
 
             colAvoidSensor = transform.Find("ColAvoidSensor").GetComponent<NearSensor>();
@@ -26,18 +20,11 @@ namespace UnityMovementAI
 
         void FixedUpdate()
         {
-            path.Draw();
-
-            if (followPath.IsAtEndOfPath(path))
-            {
-                path.ReversePath();
-            }
-
             Vector3 accel = colAvoid.GetSteering(colAvoidSensor.targets);
 
             if (accel.magnitude < 0.005f)
             {
-                accel = followPath.GetSteering(path);
+                //accel = followPath.GetSteering(path);
             }
 
             steeringBasics.Steer(accel);
---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    private int _goal = 3;
+    private int _currentAmount;
+
+    void Start()
+    {
+        _currentAmount = 0; 
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.gameObject.layer == 10)
+        {
+            Debug.Log("Stored Fish");
+
+            _currentAmount += 1;
+
+            if (_currentAmount == _goal)
+            {
+                Debug.Log("U won");
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+
+        if (col.transform.gameObject.layer == 10)
+        {
+            Debug.Log("Released fish");
+            _currentAmount -= 1;
+        }
+    }
+}
