2680e8504cdcae190a770f8e748981833d506752
samjerry <jerry.sam@ziggo.nl>
2019-11-28 15:39:41 +0100

    changed FOV script a little bit

--- Assets/Scripts/Fish/FOV.cs
+++ Assets/Scripts/Fish/FOV.cs
@@ -34,18 +34,27 @@ public class FOV : MonoBehaviour {
 
 	void FindVisibleTargets(){
 		visibleTargets.Clear();
-		Collider[] targetsInViewRadius = Physics.OverlapSphere (transform.position, viewRadius, targetMask);
 
-		for (int i = 0; i < targetsInViewRadius.Length; i++) {
-			Transform target = targetsInViewRadius [i].transform;
-			Vector3 dirToTarget = (target.position - transform.position).normalized;
-			if(Vector3.Angle (transform.forward, dirToTarget) < viewAngle / 2){
-				float dstToTarget = Vector3.Distance (transform.position, target.position);
-                    
-				if(!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask)){
-					visibleTargets.Add(target);
-				}
-			}
-		}
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        foreach (var _tar in targetsInViewRadius)
+        {
+            for (int i = 0; i < targetsInViewRadius.Length; i++)
+            {
+                if (_tar.transform.root != transform)
+                {
+                    Transform target = targetsInViewRadius[i].transform;
+                    Vector3 dirToTarget = (target.position - transform.position).normalized;
+                    if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+                    {
+                        float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                        if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                        {
+                            visibleTargets.Add(target);
+                        }
+                    }
+                }
+            }
+        }
 	}
 }
