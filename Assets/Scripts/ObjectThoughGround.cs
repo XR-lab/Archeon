8394efba537aaacc4ee238ca63b08a2a0970bd31
gwnrobin <robinschellingerhout@live.nl>
2019-11-29 14:10:37 +0100

    test new pek

---
+++ Assets/Scripts/ObjectThoughGround.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectThoughGround : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider _col)
+    {
+        _col.transform.position = new Vector3(_col.transform.position.x, 1, _col.transform.position.z);
+    }
+}
