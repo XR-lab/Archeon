caf27aff7688dddc68f3fd43aa17f5910f635c25
unknown <jerry.sam@ziggo.nl>
2019-12-03 17:03:16 +0100

    Added forrest bg sound, Added river sound, Added spear pickup sound

--- Assets/Scripts/spear-stabbing/SpearGrab.cs
+++ Assets/Scripts/spear-stabbing/SpearGrab.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class SpearGrab : MonoBehaviour {
     private Rigidbody _rb;
     private SpearPointCollision _point;
+    private AudioSource _audioData;
 
     void Start() {
         _rb = GetComponent<Rigidbody>();
         _point = GetComponentInChildren<SpearPointCollision>();
+        _audioData = GetComponent<AudioSource>();
     }
 
     public void OnPickUp() {
         _point.Grabbed = true;
         _rb.constraints = RigidbodyConstraints.None;
+        _audioData.Play(0);
+        
     }
 
     public void OnDrop() {
