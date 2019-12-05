6f871cffcb029cd5ce7bf36f00def39d8d9e5be4
krokoloko <daan.j.ruting@gmail.com>
2019-12-05 13:42:59 +0100

    Added feedback on the wincondition
    you will hear sound when you throw the fish in the 'basket'. Also changed the wincondition script a bit to make it more compatible with event triggers.

---
+++ Assets/Scripts/Dialogue System/DialogueSequence.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSequence : MonoBehaviour
+{
+    [SerializeField]
+    private string _id;
+
+    public string Id
+    {
+        get { return _id; }
+        private set { _id = Id; }
+    }
+
+    [SerializeField]
+    private List<DialogueAction> _actionSequence;
+
+    public IEnumerator ActivateSequence()
+    {
+        foreach (DialogueAction action in _actionSequence)
+        {
+            yield return new WaitForEndOfFrame();
+
+            StartCoroutine(action.Action(action.delay));
+        }
+    }
+}
---
+++ Assets/Scripts/WinEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinEvent : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip _pointSoundEffect;
+    [SerializeField]
+    private AudioClip _winSoundEffect;
+
+    private AudioSource _source;
+
+    private void Start()
+    {
+        _source = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void PointAdded()
+    {
+        _source.clip = _pointSoundEffect;
+        _source.Play();
+    }
+
+    public void PointsAccomplished()
+    {
+        _source.clip = _winSoundEffect;
+        _source.Play();
+    }
+}
