4330739e44464ced0c93010594296b8691a58e88
gwnrobin <robinschellingerhout@live.nl>
2019-12-17 14:23:04 +0100

    hand shake

--- Assets/Scripts/AnimateShader.cs
+++ Assets/Scripts/AnimateShader.cs
@@ -4,38 +4,33 @@ using UnityEngine;
 
 public class AnimateShader : MonoBehaviour
 {
-    MeshRenderer _meshRenderer;
-    SkinnedMeshRenderer _skinnedRenderer;
     Material _mat;
-    Material _skinMat;
 
     bool _animation = false;
 
-    void Start()
+    private void Start() 
     {
-        _meshRenderer = GetComponent<MeshRenderer>();
-        _skinnedRenderer = GetComponent<SkinnedMeshRenderer>();
-        if (_meshRenderer != null)
-        {
-            _mat = _meshRenderer.material;
-        }
-        if (_skinMat != null)
-        {
-            _skinMat = _skinnedRenderer.material;
-        }
-        StartRoutine();
+        _mat = GetComponent<Renderer>().material;
     }
 
-    public void StartRoutine()
+    public void StartRoutineUp()
     {
-        if (!_animation)
+        if (true)
         {
-            StartCoroutine(Animate());
+            StartCoroutine(AnimateUp());
         }
         _animation = true;
     }
 
-    IEnumerator Animate()
+    public void StartRoutineDown() {
+        if (true) 
+        {
+            StartCoroutine(AnimateDown());
+        }
+        _animation = true;
+    }
+
+    IEnumerator AnimateUp()
     {
         float _value = 0f;
         while (_value < 1f)
@@ -44,7 +39,18 @@ public class AnimateShader : MonoBehaviour
             _value = Mathf.Clamp01(_value);
 
             _mat?.SetFloat("_value", _value);
-            _skinMat?.SetFloat("_value", _value);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator AnimateDown() {
+        float _value = 1f;
+        while (_value > 0f) {
+            _value -= Time.deltaTime;
+            _value = Mathf.Clamp01(_value);
+
+            _mat?.SetFloat("_value", _value);
 
             yield return null;
         }
---
+++ Assets/Scripts/FireGhostRange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGhostRange : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+    [SerializeField]
+    private float _onRange = 1f;
+    private bool _bool = true;
+
+    private void Update() 
+    {
+        float _dist = Vector3.Distance(this.transform.position, _player.transform.position);
+        if(_dist < _onRange) 
+        {
+            this.GetComponent<AnimateShader>().StartRoutineDown();
+        }
+        else 
+        {
+            this.GetComponent<AnimateShader>().StartRoutineUp();
+        }
+    }
+}
